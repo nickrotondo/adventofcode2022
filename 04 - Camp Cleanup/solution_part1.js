91d704066f0f223797e9a8188facc0a7b94d4c6f
Nick Rotondo <54091445+nickrotondo@users.noreply.github.com>
2022-12-04 00:26:20 -0600

    Prompt and solution for day 04

---
+++ 04 - Camp Cleanup/solution_part1.js
@@ -0,0 +1,39 @@
+const readFileSync = require("fs").readFileSync;
+
+let sectionAssignmentGroups = readFileSync("./puzzle_input.txt", {
+  encoding: "utf8",
+})
+  .trim()
+  .split("\n")
+  .map((sectionAssignments) =>
+    sectionAssignments
+      .split(",")
+      .map((sectionAssignment) => sectionAssignment.split("-").map(Number))
+  );
+
+// Check what the input looks like
+// sectionAssignmentGroups.forEach((element) => {
+//   console.log(element);
+// });
+
+// Check what the input looks like
+// console.log({ sectionAssignmentGroups });
+
+function findRedundantAssignments(sectionAssignmentGroups) {
+  let count = 0;
+
+  sectionAssignmentGroups.forEach((sectionAssignments) => {
+    if (
+      (sectionAssignments[0][0] <= sectionAssignments[1][0] &&
+        sectionAssignments[0][1] >= sectionAssignments[1][1]) ||
+      (sectionAssignments[1][0] <= sectionAssignments[0][0] &&
+        sectionAssignments[1][1] >= sectionAssignments[0][1])
+    ) {
+      count++;
+    }
+  });
+
+  return count;
+}
+
+console.log(findRedundantAssignments(sectionAssignmentGroups));
